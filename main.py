c82b8111d33b3f6fac3e8b5f53dbf76b4c68853b
Mateus Fiori <mateus.fiori@gruponewway.com.br>
2019-09-05 23:30:05 -0300

    Adicionada função para gerar resultado estruturado em json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from luminol.anomaly_detector import AnomalyDetector
 from datetime import datetime
 import pandas as pd
+import pprint
 
 def parseTs(dataArray): 
     dataDict = dict()
@@ -11,10 +12,22 @@ def parseTs(dataArray):
 def formatAnomalies(anomalies):
     return [[anomaly.exact_timestamp, anomaly.anomaly_score] for anomaly in anomalies]
 
-def formatResult(anomalies, timeseriesDict):
+def verboseResult(anomalies, timeseriesDict):
     print(f'Foram encontradas {len(anomalies)} anomalias na sua série temporal.\n')
     for anomaly in anomalies:
         print(f'No timestamp {datetime.fromtimestamp(anomaly[0])} com um score de {anomaly[1]} e valor {timeseriesDict[str(anomaly[0])]}')
 
+def generateJsonResult(anomalies, timeseriesDict):
+    result = {'anomalies': list()}
+    for anomaly in anomalies:
+        result['anomalies'].append({
+            'timestamp': str(datetime.fromtimestamp(anomaly[0])),
+            'value': timeseriesDict[str(anomaly[0])],
+            'anomaly_score': anomaly[1]
+        })
+    # pprint.PrettyPrinter(indent=3).pprint(result)
+    return result
+
 timeseriesDict = parseTs(pd.read_csv('./data/data.csv').values)
-formatResult(formatAnomalies(AnomalyDetector(timeseriesDict).get_anomalies()), timeseriesDict)
+verboseResult(formatAnomalies(AnomalyDetector(timeseriesDict).get_anomalies()), timeseriesDict)
+generateJsonResult(formatAnomalies(AnomalyDetector(timeseriesDict).get_anomalies()), timeseriesDict)
